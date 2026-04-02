ad0c3d5a9525fbdeef5ca8399ac683c2b1fdc22b
ligarius20fps <ligarius20fps@users.noreply.github.com>
2026-04-02 14:32:01 +0200

    chrupanie

--- podstawy/Scenes/Ocean/Ocean.gd
+++ podstawy/Scenes/Ocean/Ocean.gd
@@ -2,12 +2,11 @@ extends Node2D
 
 @onready var samolot: Sprite2D = $Plane
 @onready var heli: Sprite2D = $Helicopter
-
+@onready var chrup: AudioStreamPlayer = $chrup
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
-
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
@@ -21,8 +20,13 @@ func _process(delta: float) -> void:
 	# obracaj samolotem
 	if Input.is_action_pressed("ui_left"):
 		samolot.rotate(-rotate_speed * delta)
+	
 	if Input.is_action_pressed("ui_right"):
 		samolot.rotate(rotate_speed * delta)
+	
+	# chrupaj gdy naciskam spację
+	if Input.is_action_just_pressed("ui_select" ):
+		chrup.play()
 	
 	# ćwiczenie: helikopter lata w kółko
 	heli.rotate(rotate_speed / 8 * delta)
