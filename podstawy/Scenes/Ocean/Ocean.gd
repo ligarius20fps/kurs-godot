89e933afa31fe6f31048bda0900adcc5b5e0d399
ligarius20fps <ligarius20fps@users.noreply.github.com>
2026-04-03 11:41:16 +0200

    samolot śledzi kursor, moim sposobem

--- podstawy/Scenes/Ocean/Ocean.gd
+++ podstawy/Scenes/Ocean/Ocean.gd
@@ -4,6 +4,7 @@ extends Node2D
 @onready var heli: Sprite2D = $Helicopter
 @onready var chrup: AudioStreamPlayer = $chrup
 const speed:int = 200
+const wind_speed:int = 60
 const PI:float = 3.141592653589793238462643383
 const rotate_speed:float = PI
 
@@ -15,7 +16,15 @@ func _ready() -> void:
 func _process(delta: float) -> void:
 	# przemieszczaj latające maszyny
 	heli.move_local_x(speed * delta * 0.5)
-	samolot.global_position.x += 60 * delta
+	samolot.move_local_x(speed * delta)
+	#samolot.global_translate(Vector2.RIGHT.normalized() * wind_speed * delta)
+	
+	# samolot śledzi kursor
+	var v1:Vector2 = samolot.global_position
+	var v2:Vector2 = get_global_mouse_position()
+	var angl = (v2 - v1).angle()
+	samolot.rotation = angl 
+	
 	if Input.is_action_pressed("ui_up"):
 		samolot.move_local_x(speed * delta)
 	
@@ -28,8 +37,6 @@ func _process(delta: float) -> void:
 	if Input.is_action_just_pressed("ui_select"):
 		# chrupaj gdy naciskam spację
 		chrup.play()
-		# i przemieść samolot do helikoptera
-		samolot.global_position  = Vector2(heli.position.x, heli.position.y)
 	
 	# ćwiczenie: helikopter lata w kółko
-	heli.rotate(rotate_speed / 8 * delta)
+	heli.rotate(-rotate_speed / 7 * delta)
