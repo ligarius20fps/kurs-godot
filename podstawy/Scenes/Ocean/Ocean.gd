019dba43e23cef1c78e1d5d07c53737bf8fa951e
ligarius20fps <ligarius20fps@users.noreply.github.com>
2026-04-03 12:04:51 +0200

    śledzenie kursora, jeszcze lepszym sposobem

--- podstawy/Scenes/Ocean/Ocean.gd
+++ podstawy/Scenes/Ocean/Ocean.gd
@@ -18,12 +18,8 @@ func _process(delta: float) -> void:
 	heli.move_local_x(speed * delta * 0.5)
 	samolot.move_local_x(speed * delta)
 	#samolot.global_translate(Vector2.RIGHT.normalized() * wind_speed * delta)
-	
 	# samolot śledzi kursor
-	var v1:Vector2 = samolot.global_position
-	var v2:Vector2 = get_global_mouse_position()
-	var angl = (v2 - v1).angle()
-	samolot.rotation = angl 
+	samolot.look_at(get_global_mouse_position())
 	
 	if Input.is_action_pressed("ui_up"):
 		samolot.move_local_x(speed * delta)
