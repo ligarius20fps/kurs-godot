c3e51894c9a5707cc09dd7dba3faece9fccf96fb
ligarius20fps <ligarius20fps@users.noreply.github.com>
2026-04-04 14:16:29 +0200

    słownik

--- podstawy/Scripts/ControlFlow.gd
+++ podstawy/Scripts/ControlFlow.gd
@@ -4,9 +4,13 @@ extends EditorScript
 
 # Called when the script is executed (using File -> Run in Script Editor).
 func _run() -> void:
-	var tab: Array = [1,5,"siema",6,3,7]
+	var tab: Array[Variant] = [1,5,"siema",6,3,7]
 	for i in tab:
 		print(i)
 	
 	var my_tab: Array =  range(1, 6, 2)
 	print(my_tab)
+	
+	var my_dict: Dictionary = {1: 2, 3: 4}
+	tab.assign(my_tab)
+	print(tab)
