616f888ab37ad9bc1e32f9f7f7706c97a0364204
ligarius20fps <ligarius20fps@users.noreply.github.com>
2026-04-05 16:07:33 +0200

    lepsze gettery/settery

--- podstawy/AdvScripts/Character.gd
+++ podstawy/AdvScripts/Character.gd
@@ -1,26 +1,40 @@
 class_name Character
 
-var _name: String = "Jeff"
-var _weapon: String = "Stick"
-var _age: int = 25
-var _health: int = 100
+var name: String = "Jeff":
+	get:
+		return name
+	set(new_name):
+		name = new_name
+
+var weapon: String = "Stick":
+	get:
+		return weapon
+	set(new_weapon):
+		weapon = new_weapon
+
+var health: int = 100:
+	get:
+		print("health getter invoked")
+		return health
+	set(new_health):
+		print("health setter invoked(%d)" % new_health)
+		health = clamp(new_health, 0, 100)
 
 func print_health():
-	print("%s's HP: %d" % [_name, _health])
+	print("%s's HP: %d" % [name, health])
 
 func say_info():
-	print("%s(%d)" % [_name, _age])
-	print("Weapon: %s" % _weapon)
-	print("HP: %s" % _health)
+	print("%s" % name)
+	print("Weapon: %s" % weapon)
+	print("HP: %s" % health)
 
 func get_health() -> int:
-	return _health
+	return health
 
 func set_health(health: int) -> void:
-	_health = clamp(health, 0, 100)
+	health = clamp(health, 0, 100)
 
-func _init(name:String = "Jeff", health: int = 100, age: int = 25, weapon: String = ""):
-	_name = name
-	_age = age
-	_health = clamp(health, 0, 100)
-	_weapon = weapon
+func _init(name:String, health: int, weapon: String):
+	self.name = name
+	self.health = health
+	self.weapon = weapon
--- podstawy/AdvScripts/Runner.gd
+++ podstawy/AdvScripts/Runner.gd
@@ -3,24 +3,33 @@ extends EditorScript
 
 # Called when the script is executed (using File -> Run in Script Editor).
 func _run() -> void:
-	var jeff: Character = Character.new()
-	jeff.print_health()
-	jeff._health = 105
-	jeff.print_health()
-	print(jeff.get_instance_id())
-	jeff.say_info()
+	#var jeff: Character = Character.new()
+	#jeff.print_health()
+	#jeff.health = 105
+	#jeff.print_health()
+	#print(jeff.get_instance_id())
+	#jeff.say_info()
+	#
+	#var bob = Character.new("Bob", 80, 17, "Gun")
+	#bob.say_info()
+	#print(bob.get_instance_id())
+	#print("Bob: OW !")
+	#bob.health -= 20
+	#bob.print_health()
+	#
+	#var steve:Character = Character.new("Steve", 105, 16, "Sword")
+	#steve.say_info()
+	#while steve.get_health() > 0:
+		#steve.set_health(steve.get_health() - range(17, 24).pick_random())
+		#steve.print_health()
+	#if steve.get_health() <= 0:
+		#print("%s is dead by now" % steve._name)
+		
+	var johnny: Character = Character.new("Johnny", 2000, "Bare fists")
+	johnny.say_info()
+	while johnny.health > 0:
+		johnny.health = johnny.health - range(17, 24).pick_random()
+		johnny.print_health()
+	if johnny.health <= 0:
+		print("%s is dead by now" % johnny.name)
 	
-	var bob = Character.new("Bob", 80, 17, "Gun")
-	bob.say_info()
-	print(bob.get_instance_id())
-	print("Bob: OW !")
-	bob._health -= 20
-	bob.print_health()
-	
-	var steve:Character = Character.new("Steve", 105, 16, "Sword")
-	steve.say_info()
-	while steve.get_health() > 0:
-		steve.set_health(steve.get_health() - range(17, 24).pick_random())
-		steve.print_health()
-	if steve.get_health() <= 0:
-		print("Steve is dead by now")
