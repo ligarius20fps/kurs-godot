7cba371782d1ac2c2218318e9ebb5ca41cf78355
ligarius20fps <ligarius20fps@users.noreply.github.com>
2026-04-05 14:35:40 +0200

    zadanie z gettera/settera

--- podstawy/AdvScripts/Character.gd
+++ podstawy/AdvScripts/Character.gd
@@ -6,15 +6,21 @@ var age: int = 25
 var health: int = 100
 
 func print_health():
-	print("%s's HP: %d" % [name, health])
+	print("%s's HP: %d" % [self.name, self.health])
 
 func say_info():
-	print("%s(%d)" % [name, age])
-	print("Weapon: %s" % weapon)
-	print("HP: %s" % health)
+	print("%s(%d)" % [self.name, self.age])
+	print("Weapon: %s" % self.weapon)
+	print("HP: %s" % self.health)
 
-func _init(new_name:String = "Jeff", new_health: int = 100, new_age: int = 25, new_weapon: String = ""):
-	name = new_name
-	age = new_age
-	health = new_health
-	weapon = new_weapon
+func get_health() -> int:
+	return self.health
+
+func set_health(health: int) -> void:
+	self.health = clamp(health, 0, 100)
+
+func _init(name:String = "Jeff", health: int = 100, age: int = 25, weapon: String = ""):
+	self.name = name
+	self.age = age
+	self.health = clamp(health, 0, 100)
+	self.weapon = weapon
--- podstawy/AdvScripts/Runner.gd
+++ podstawy/AdvScripts/Runner.gd
@@ -3,12 +3,24 @@ extends EditorScript
 
 # Called when the script is executed (using File -> Run in Script Editor).
 func _run() -> void:
-	var my_char: Character = Character.new()
-	my_char.print_health()
-	my_char.health = 75
-	my_char.print_health()
-	print(my_char.get_instance_id())
-	my_char.say_info()
-	var new_char = Character.new("Bob", 80, 17, "Gun")
-	new_char.say_info()
-	print(new_char.get_instance_id())
+	var jeff: Character = Character.new()
+	jeff.print_health()
+	jeff.health = 105
+	jeff.print_health()
+	print(jeff.get_instance_id())
+	jeff.say_info()
+	
+	var bob = Character.new("Bob", 80, 17, "Gun")
+	bob.say_info()
+	print(bob.get_instance_id())
+	print("Bob: OW !")
+	bob.health -= 20
+	bob.print_health()
+	
+	var steve:Character = Character.new("Steve", 105, 16, "Sword")
+	steve.say_info()
+	while steve.get_health() > 0:
+		steve.set_health(steve.get_health() - range(17, 24).pick_random())
+		steve.print_health()
+	if steve.get_health() <= 0:
+		print("Steve is dead by now")
