a2caa86bccbd560fa99501af7a2d46c02678c44f
ligarius20fps <ligarius20fps@users.noreply.github.com>
2026-04-11 17:12:28 +0200

    pierwszy sygnał

--- dice-catcher/Scenes/Hero/Hero.gd
+++ dice-catcher/Scenes/Hero/Hero.gd
@@ -1,6 +1,6 @@
 class_name Hero
 extends Node2D
-
+"""
 var p_f: int = 0
 var ph_f: int = 0
 
@@ -12,18 +12,18 @@ func _enter_tree() -> void:
 
 func _exit_tree() -> void:
 	print("_exit_tree(%s %s)" % [get_instance_id(), name])
-
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	print("_ready(%s %s)" % [get_instance_id(), name])
-
-
-# knajt się kręci
-func _process(delta: float) -> void:
-	p_f += 1
-	print("_process %d" % p_f)
-	rotate(4* delta)
-
+	
 func _physics_process(_delta: float) -> void:
 	ph_f += 1
 	print("_physics_process %d" % ph_f)
+"""
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass
+
+# knajt się kręci
+func _process(delta: float) -> void:
+	#p_f += 1
+	#print("_process %d" % p_f)
+	rotate(PI* delta)
--- dice-catcher/Scenes/Wizard/Wizard.gd
+++ dice-catcher/Scenes/Wizard/Wizard.gd
@@ -3,12 +3,15 @@ extends Node2D
 
 const MAX_SCALE = 2
 
+@onready var appear_timer: Timer = $AppearTimer
+
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	hide()
 
-#magik się spłaszcza
 func _process(delta: float) -> void:
+	#magik się spłaszcza
 	var scale_transform = Vector2(transform.x.x, transform.y.y)
 	scale_transform.y += delta
 	if abs(scale_transform.y) >= 2:
@@ -16,3 +19,7 @@ func _process(delta: float) -> void:
 	scale_transform.x = MAX_SCALE - scale_transform.y
 	transform.x.x = scale_transform.x
 	transform.y.y = scale_transform.y
+
+#pojawia się po 2 sekundach
+func _on_appear_timer_timeout() -> void:
+	show()
