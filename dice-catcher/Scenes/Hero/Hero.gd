40eadae43714b6bfd383fe4ac7b5f9df751a5f40
ligarius20fps <ligarius20fps@users.noreply.github.com>
2026-04-11 17:32:08 +0200

    eksport zmiennej

--- dice-catcher/Scenes/Hero/Hero.gd
+++ dice-catcher/Scenes/Hero/Hero.gd
@@ -1,6 +1,10 @@
 class_name Hero
 extends Node2D
 
+## po ilu sekundach znika knajt
+@export var hide_sec:float = 2.0
+@onready var hide_timer: Timer = $HideTimer
+
 """
 var p_f: int = 0
 var ph_f: int = 0
@@ -21,7 +25,7 @@ func _physics_process(_delta: float) -> void:
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass
+	hide_timer.start(hide_sec)
 
 # knajt się kręci
 func _process(delta: float) -> void:
